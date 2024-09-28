dd1d8cf6adb578797ccdce16888b65c3d78b4dea
Jeremie-mengi <jeremiemengi248@gmail.com>
2024-09-28 10:00:33 +0100

    Five

--- index.js
+++ index.js
@@ -51,3 +51,27 @@ function splitPairs(str) {
 }
 console.log(splitPairs("123456789"));
 
+// 5. Écrivez une fonction qui prend une chaîne 
+// d'un ou plusieurs mots et renvoie la même chaîne, 
+// mais avec tous les mots qui ont cinq lettres 
+// ou plus inversés (tout comme le nom de ce Kata). 
+// Les chaînes transmises seront constituées uniquement 
+// de lettres et d'espaces. Les espaces ne seront inclus 
+// que lorsque plusieurs mots sont présents.
+
+// Exemples :
+
+// "Hey fellow warriors"  --> "Hey wollef sroirraw" 
+// "This is a test        --> "This is a test" 
+// "This is another test" --> "This is rehtona test"
+function InverseMot(chaine) {
+  // Séparer la chaîne en mots
+        return  chaine.split(' ')
+  // Inverser les mots de 5 lettres ou plus
+  .map(word=>word.length>= 5 ? word.split('').reverse().join(''):word)
+  .join(' ')
+}
+console.log(InverseMot("This is another test"));
+
+
+
